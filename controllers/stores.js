cb2d3257b54447fb3080603aca4e9526a5524f08
yohan yoo <public.afant@gmail.com>
2019-05-16 19:58:09 +0900

    API Step 1

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,13 +1,57 @@
-const models = require("../models");
+const db = require("../models");
+const axios = require("axios");
+const phoneFormater = require("phone-number-formats");
+
+phoneFormater.addType("korea", "0X-XXXX-XXXX");
 module.exports = {
   list: {
     get: (req, res) => {
-      res.send("GET /stores/list OK");
+      db.stores
+        .findAll()
+        .then(result => {
+          let setList = [];
+          result.map(data => {
+            setList.push(data.dataValues);
+          });
+          console.log("setList :: ", setList);
+          res.send(setList);
+        })
+        .catch(err => {
+          console.log("ERROR :: GET /stores/list :: ", err);
+        });
     }
   },
   info: {
     get: (req, res) => {
-      res.send("GET /stores/info OK");
+      const countryCode = req.query.countryCode;
+      const storeCode = req.query.storeCode;
+
+      let url =
+        `https://www.apple.com/${countryCode}` +
+        `/shop/retail/pickup-message?parts.0=MMQA2KH/A&store=${storeCode}`;
+
+      // MMQA2J/A
+
+      console.log(url);
+
+      axios.get(url).then(result => {
+        const getStore = result.data.body.stores[0];
+        let contact;
+        if (countryCode === "kr") {
+          contact = new phoneFormater(getStore.phoneNumber);
+          contact.format({ type: "korea" });
+        } else {
+          contact = getStore.phoneNumber;
+        }
+        let storeInfoData = {
+          storeName: getStore.storeName,
+          address: getStore.address,
+          contact: contact,
+          image_url: getStore.storeImageUrl
+        };
+        // console.log(storeInfoData);
+        res.json(storeInfoData);
+      });
     }
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,10 @@ var express = require("express");
 var router = express.Router();
 const controllers = require("../controllers");
 
-router.get("/signin", controllers.users.signin.get);
+router.post("/auth", controllers.users.auth.post);
 router.post("/signup", controllers.users.signup.post);
+router.post("/token", controllers.users.token.post);
+router.delete("/delete", controllers.users.delete.delete);
+router.delete("/deletebyid", controllers.users.deletebyid.delete);
 
 module.exports = router;
