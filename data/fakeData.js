250f4e24027ae699cd80af8b7af02e1bb62020de
yohan yoo <public.afant@gmail.com>
2019-05-17 19:18:32 +0900

    Make FakeData for /products/list

---
+++ data/fakeData.js
@@ -0,0 +1,64 @@
+module.exports = {
+  productlist: [
+    {
+      productID: 1,
+      storeID: 1,
+      modelCode: "MRE82KH/A",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/air/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1541713862468",
+      isPickupAvailable: true,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: false
+    },
+    {
+      productID: 2,
+      storeID: 1,
+      modelCode: "MPXQ2KH/A",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/m/bp/mbp13/space/mbp13-space-select-201807_GEO_KR?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1530049921050",
+      isPickupAvailable: true,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: true
+    },
+    {
+      productID: 3,
+      storeID: 1,
+      modelCode: "MTXP2KH/A",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/pro/ipad-pro-11-select-wifi-silver-201810_GEO_KR?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1549066558625",
+      isPickupAvailable: false,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: false
+    },
+    {
+      productID: 4,
+      storeID: 1,
+      modelCode: "MT9F2KH/A",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/xs/iphone-xs-silver-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795411708",
+      isPickupAvailable: true,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: false
+    },
+    {
+      productID: 5,
+      storeID: 1,
+      modelCode: "MU6A2KH/A",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/4/4/44/alu/44-alu-silver-sport-white-nc-s4-1up_GEO_KR?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1539190326497",
+      isPickupAvailable: true,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: true
+    }
+  ]
+};
