a971953571495e275412e61196fdd883f663099b
yohan yoo <public.afant@gmail.com>
2019-05-17 14:51:48 +0900

    /users/signup Check valid user

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,6 @@ module.exports = {
   list: {
     get: (req, res) => {
       let query = req.query;
-      console.log("/products/list QUERY::: ", query);
       res.send("GET /products/list OK");
     }
   }
