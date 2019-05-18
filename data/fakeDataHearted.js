aeedf9974ab7b48746207c8ba286ef9fc908d30c
yohan yoo <public.afant@gmail.com>
2019-05-18 19:41:49 +0900

    Make fakeDataHearted

---
+++ data/fakeDataHearted.js
@@ -0,0 +1,43 @@
+module.exports = {
+  productlist: [
+    {
+      productID: 3,
+      storeID: 1,
+      modelCode: "MTXP2KH/A",
+      modelName: "iPad Pro 3/11/64/S/WIFI",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/pro/ipad-pro-11-select-wifi-silver-201810_GEO_KR?wid=200&hei=200&fmt=png-alpha&qlt=80&.v=1549066558625",
+      isPickupAvailable: false,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: true
+    },
+    {
+      productID: 4,
+      storeID: 1,
+      modelCode: "MT9F2KH/A",
+      modelName: "iPhoneXS 64/S",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/xs/iphone-xs-silver-select-2018?wid=200&hei=200&fmt=png-alpha&qlt=80&.v=1550795411708",
+      isPickupAvailable: true,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: true
+    },
+    {
+      productID: 5,
+      storeID: 1,
+      modelCode: "MU6A2KH/A",
+      modelName: "Apple Watch 4/44/S/Alu",
+      imageUrl:
+        "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/4/4/44/alu/44-alu-silver-sport-white-nc-s4-1up_GEO_KR?wid=200&hei=200&fmt=png-alpha&qlt=80&.v=1539190326497",
+      isPickupAvailable: true,
+      storeCode: "R692",
+      countryCode: "kr",
+      storeName: "Apple 가로수길",
+      isHearted: true
+    }
+  ]
+};
