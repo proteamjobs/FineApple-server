61cb45e002442548cedcb5399ea24a6fcaca8a8f
yohan yoo <public.afant@gmail.com>
2019-05-18 20:14:32 +0900

    Add fakeDataStoreInfo

---
+++ data/fakeDataStoreInfo.js
@@ -0,0 +1,21 @@
+module.exports = {
+  storeName: "APPLE 가로수길",
+  address: {
+    address: "Apple 가로수길",
+    address3: "압구정로 12길 46",
+    address2: "강남구",
+    postalCode: "06028"
+  },
+  contact: "02-1234-1234",
+  image_url:
+    "https://rtlimages.apple.com/cmc/dieter/store/4_3/R692.png?resize=828:*&output-format=jpg",
+  way_to_come: "way_to_come",
+  storeHours: {
+    storeTimings: "10:00 오전-10:00 오후",
+    storeDays: "월요일-일요일:"
+  },
+  storeLocation: {
+    storelatitude: 37.5209,
+    storelongitude: 127.0228
+  }
+};
