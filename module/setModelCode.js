87de7cc4359284f133e35c99498777e68f7f8f8c
yohan yoo <public.afant@gmail.com>
2019-05-18 15:29:26 +0900

    Change setModelCode Logic

--- module/setModelCode.js
+++ module/setModelCode.js
@@ -1,18 +1,14 @@
-module.exports = (model_code, country_code) => {
+module.exports = country_code => {
   // ex 1) JP -> MMQA2"K"/A -> MMQA2"J"/A
   // ex 2) CN -> MMQA2"K"/A -> MMQA2"CH"/A
   const changeCode = {
-    kr: "kh",
-    jp: "j",
-    hk: "zp"
+    kr: "MRE82KH/A",
+    jp: "MRE82J/A",
+    hk: "MRE82ZP/A"
   };
-  let modelCodePartA = model_code.slice(0, 5);
-  let modelCodePartB = model_code.slice(7, 9);
-
   for (let key in changeCode) {
-    console.log(key);
     if (key === country_code) {
-      return modelCodePartA + changeCode[key] + modelCodePartB;
+      return changeCode[key];
     }
   }
 };
